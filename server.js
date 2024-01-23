be2b632d6a477be0892f44caacb39ca7054001fa
Michael Grauer <michael.grauer@gmail.com>
2024-01-23 16:49:11 -0500

    Add ability to create a new todo list

--- server.js
+++ server.js
@@ -4,15 +4,25 @@ const port = 3000;
 
 // Serve static files from the public directory
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-// Define your endpoints here
+let todo_lists = ['jobs', 'house', 'personal'];
+
+// Endpoints
+
+
+
 app.get('/api/lists', (req, res) => {
-    // Handle the GET request here
-    const items = ['item_1', 'item_2', 'item_3'];
-    const html = `${items.map(item => `<li>${item}</li>`).join('')}`;
+    const html = `${todo_lists.map(item => `<li>${item}</li>`).join('')}`;
     res.send(html);
 });
 
+app.post('/api/lists', (req, res) => {
+    const newList = req.body.name; // Assuming the new string is passed in the request body as "list"
+    todo_lists.push(newList); // Add the new string to the todo_lists array
+    const html = `<li>${newList}</li>`;
+    res.send(html);
+});
 
 // Start the server
 app.listen(port, () => {
