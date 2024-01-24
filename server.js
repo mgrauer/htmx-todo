0aa1e0f7f606226e400364b54d8d110c168a5ca2
Michael Grauer <michael.grauer@gmail.com>
2024-01-24 11:58:51 -0500

    Enable todo list name editing

--- server.js
+++ server.js
@@ -6,20 +6,76 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-let todo_lists = ["jobs", "house", "personal"];
+// Model
+class TodoList {
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+  }
+
+  getHtml() {
+    return `
+      <li>
+        <form hx-put="/api/lists/${this.id}" hx-trigger="submit" hx-target="closest li" hx-swap="outerHTML">
+          <input type="text" name="name" value="${this.name}" required />
+          <button type="submit">Save</button>
+        </form>
+      </li>
+    `;
+  }
+}
+
+class TodoLists {
+  constructor() {
+    this.todoLists = [
+      new TodoList("jobs", 1),
+      new TodoList("house", 2),
+      new TodoList("personal", 3),
+    ];
+  }
+
+  getHtml() {
+    return this.todoLists.map((item) => item.getHtml()).join("");
+  }
+
+  addList(newList) {
+    const id = this.todoLists.length + 1;
+    const todoList = new TodoList(newList, id);
+    this.todoLists.push(todoList);
+    return todoList;
+  }
+}
+// View
+
+// Controller
 
 // Endpoints
+const todoLists = new TodoLists();
 
 app.get("/api/lists", (req, res) => {
-  const html = `${todo_lists.map((item) => `<li>${item}</li>`).join("")}`;
+  const html = todoLists.getHtml();
   res.send(html);
 });
 
 app.post("/api/lists", (req, res) => {
-  const newList = req.body.name; // Assuming the new string is passed in the request body as "list"
-  todo_lists.push(newList); // Add the new string to the todo_lists array
-  const html = `<li>${newList}</li>`;
+  const newList = req.body.name; // Assuming the new string is passed in the request body as "name"
+  const todoList = todoLists.addList(newList);
+  const html = todoList.getHtml();
   res.send(html);
+});
+
+app.put("/api/lists/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const newName = req.body.name;
+
+  const todoList = todoLists.todoLists.find((list) => list.id === id);
+  if (todoList) {
+    todoList.name = newName;
+    const html = todoList.getHtml();
+    res.send(html);
+  } else {
+    res.status(404).json({ message: "Todo list not found" });
+  }
 });
 
 // Start the server
